7cd76df3f2c972a2ea4d030b3e345746d743950c
Santhosh-Kumar22 <85182229+Santhosh-Kumar22@users.noreply.github.com>
2024-02-03 18:00:14 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>" If you aren't taking Notes, you aren't Learning. "</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ notes.jsx
@@ -0,0 +1,22 @@
+const notes =[
+    {
+        key : "1", 
+        title : "First Note",
+        content : "This is my first note." 
+    }, 
+
+    {
+        key : "2", 
+        title : "Second Note",
+        content : "This is my Second note." 
+    },
+        
+    {
+        key : "3", 
+        title : "Third Note",
+        content : "This is my third note." 
+    },    
+];
+
+export default notes;
+
